0c96be9e8dd89be765b05f515c256049d950d961
azizsheq <azizsheq@gmail.com>
2021-03-09 09:18:13 +0800

    first commit and testing connection with git

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+      <Button>Bootstrap</Button>
+    </div>
+  );
+}
+
+export default App;
